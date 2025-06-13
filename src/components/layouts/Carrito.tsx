fd0b29c782abad04674157d1895c662d194f85d6
isabella <bellabernalcarvajal871@gmail.com>
2025-06-13 12:14:35 -0500

    Menú de navegacion entre páginas2

--- src/components/layouts/Carrito.tsx
+++
@@ -1,7 +0,0 @@
-
-export const Carrito = () => {
-    return (
-        <div>
-        </div>
-    )
-}
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,12 +2,24 @@ import { NavLink } from "react-router-dom"
 
 export const Navbar = () => {
     return (
-        <div className="flex justify-center pt-25 relative z-10">
-            <div className="bg-[#32492f] rounded-[50px] w-[480px] h-13 flex justify-center items-center text-[#f5e9db] text-[20px] italic font-semibold">
+        <div className="flex justify-end pt-[2.7%] pr-10">
+            <div className="bg-[#ffffff] rounded-[50px] w-[480px] h-13 flex justify-center items-center text-[#000000] text-[15px]">
             <ul className="gap-18 flex justify-center items-center">
-                <li className="hover:scale-104 transition duration-200"><NavLink to="/">Inicio</NavLink></li>
-                <li className="hover:scale-104 transition duration-200"><NavLink to="/Libros">Libros</NavLink></li>
-                <li className="hover:scale-104 transition duration-200"><NavLink to="Recetas">Recetas</NavLink></li>
+                <li className="hover:scale-135 transition duration-200"><NavLink to="/">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="26"  height="26"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-home"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                </NavLink></li>
+                <li className="hover:scale-135 transition duration-200"><NavLink to="/Libros">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-book-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z" /><path d="M19 16h-12a2 2 0 0 0 -2 2" /><path d="M9 8h6" /></svg>
+                </NavLink></li>
+                <li className="hover:scale-135 transition duration-200"><NavLink to="Recetas">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-tools-kitchen-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3" /></svg>                  
+                </NavLink></li>
+                <li className="hover:scale-135 transition duration-200"><NavLink to="Carrito">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
+                </NavLink></li>
+                <li className="hover:scale-135 transition duration-200"><NavLink to="Usuario">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                </NavLink></li>
             </ul>
         </div>
         </div>
