1666bb1cbd3eda05432d753886e4a6a6013270a2
santiago <santiagovillacano48@gmail.com>
2025-06-20 08:54:18 -0500

    cambios de imagen a registro

--- src/components/pages/Usuario.tsx
+++
@@ -1,9 +0,0 @@
-import { Registro } from "./Registro"
-
-export const Usuario = () => {
-    return (
-        <div className="w-full h-full flex items-center justify-center ">
-            <Registro />
-        </div>
-    )
-}
