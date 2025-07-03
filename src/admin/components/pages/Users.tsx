40eab84edc33819ae482f1fd392ae6380f51f273
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-07-02 19:40:27 -0500

    fechin de datos a las apis

--- src/admin/components/pages/Users.tsx
+++ src/admin/components/pages/Users.tsx
@@ -1,8 +1,9 @@
 import { getUsers } from "../../services/Users"
 import { useEffect, useState } from "react"
+import type { user } from "../../../types/users"
 export const Users = () => {
 
-  const [users,SetUsers] = useState()
+  const [users,SetUsers] = useState<user[]>([])
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -15,9 +16,11 @@ export const Users = () => {
 
   return (
     <div>
-
-
-
+         {
+          users.map((user)=>(
+            <p>{user.nombre_usuario}</p>
+          ))
+        }
     </div>
   )
 }
