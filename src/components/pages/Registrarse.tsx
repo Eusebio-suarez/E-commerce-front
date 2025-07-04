e83872b9e22ff4249e94840664b2d0c1723595d4
Santiago villa cano <117299695+Toutrox48@users.noreply.github.com>
2025-07-04 10:47:56 -0500

    Update Registrarse.tsx

--- src/components/pages/Registrarse.tsx
+++ src/components/pages/Registrarse.tsx
@@ -108,7 +108,8 @@ const handleRegisterClick = async () => {
         </div>
         <div className='absolute py-15 w-[390px] h-[80%] max-[750px]:flex-col backdrop-blur-xm backdrop-blur-sm gap-4 rounded-2xl shadow-2xl shadow-black flex flex-row items-center content-center justify-center p-4  mt-10'>
             <div className='w-[100%] h-full flex items-center justify-center'>
-                <form onSubmit={validateForm} className='w-full max-w-sm flex flex-col items-center'>
+
+                <form className='w-full max-w-sm flex flex-col items-center'>
                     <div className='mb-4'>
                         <h3 className='text-center text-[28px] font-bold text-white'>Registrate</h3>
                         <label className='block text-gray-100 text-sm font-bold mb-2 pt-5' htmlFor='username'>
@@ -123,7 +124,7 @@ const handleRegisterClick = async () => {
                             Contraseña
                         </label>
                         <input value={formInput.password} onChange={(e) => setFormInput({...formInput, password: e.target.value})} className='bg-gray-300 w-[240px] h-[30px] pl-2 rounded-[5px] outline-none' id='password' type='password' placeholder='******************' />
-                        <p className='text-red-500 text-xs italic'>{FormErrors.password}</p> 
+                        <p className='text-red-500 text-xs italic'>{FormErrors.password}</p>
                     </div>
 
                     <div className='mb-4'>
@@ -132,7 +133,7 @@ const handleRegisterClick = async () => {
                         </label>
                         <input value={formInput.passwordConfirm} onChange={(e) => setFormInput({...formInput, passwordConfirm: e.target.value})} className='bg-gray-300 w-[240px] h-[30px] pl-2 rounded-[5px] outline-none' id='passwordconfirm' type='password' placeholder='******************' />
                         <p className='text-red-500 text-xs italic'>{FormErrors.passwordConfirm}</p>
-                        
+
                     </div>
 
                     <div className='mb-6'>
@@ -148,15 +149,16 @@ const handleRegisterClick = async () => {
                     )}
 
                     <div className='flex gap-6'>
-                        <button onClick={handleRegister} className='submit bg-yellow-500 w-[120px] h-[30px] rounded-[8px] text-white hover:bg-yellow-600 font-bold focus:outline-none focus:shadow-outline' type='button'>
+
+                        <button onClick={handleRegisterClick} className='submit bg-yellow-500 w-[120px] h-[30px] rounded-[8px] text-white hover:bg-yellow-600 font-bold focus:outline-none focus:shadow-outline' type='button'>
                             Register
                         </button>
                         <button className='submit bg-gray-100 hover:bg-gray-500 text-gray-700  hover:text-gray-100 font-bold w-[170px] h-[30px] rounded-[8px] focus:outline-none focus:shadow-outline'>
-                            Iniciar con Google 
+                            Iniciar con Google
                         </button>
                     </div>
 
-                    <p className="text-yellow-200 font-semibold pt-5">¿Ya tienes cuenta?</p> 
+                    <p className="text-yellow-200 font-semibold pt-5">¿Ya tienes cuenta?</p>
                     <NavLink to="/InicioSesion" className="font-bold text-yellow-200 no-underline hover:underline decoration-2">Inicia Sesion</NavLink>
                 </form>
             </div>
